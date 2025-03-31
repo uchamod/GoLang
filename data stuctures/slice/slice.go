d3204bb2cbb18e95623828d917b7be5439710a15
chamod udara <uchamod52@gmail.com>
2025-03-31 11:09:59 +0530

    add slice and arrays

--- data stuctures/slice/slice.go
+++ data stuctures/slice/slice.go
@@ -12,10 +12,11 @@ func main() {
 	numbers = append(numbers, 5, 6, 7)
 	fmt.Println(numbers)
 
-	nums := []int{1, 2, 3, 4, 5, 6, 7, 9}
+	nums := []int{1, 2, 3, 4, 5, 6, 7, 9} 
 	fmt.Println(nums)
 
 	nums = append(nums[:4], nums[5:]...)//use elips
 	fmt.Println(nums)
+
 
 }
--- loops/loops.go
+++ loops/loops.go
@@ -1,5 +1,48 @@
 package main
 
+import "fmt"
+
+type student struct {
+	id   int
+	name string
+}
+
+func (student) read() string {
+	return "students read books"
+}
+
+// infinity loop with limitaions
+func maxMassage(limit int) float64 {
+	totalCost := 0.0
+	for i := 0; ; i++ {
+		totalCost += 2.0 * float64(i)
+		if i >= limit {
+			return totalCost
+		}
+	}
+}
+
+// while loop : this is achive also a using for keyword
+func ageChecker(initialAge int) {
+
+	for initialAge <=  17 {
+		fmt.Println("you cannot enter")
+		initialAge++
+		if initialAge >= 18 {
+			fmt.Println("you can enter")
+			return
+		}
+		continue
+	}
+}
 func main() {
-	
+	s := student{
+		id:   001,
+		name: "chamod",
+	}
+	for i := 0; i < 5; i++ {
+		fmt.Println(s.read())
+	}
+	fmt.Println(maxMassage(10))
+	ageChecker(10)
 }
